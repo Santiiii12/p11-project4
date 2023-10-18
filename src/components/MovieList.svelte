384e85ebe42d040d0025afcef7a539e04ca1063f
Santiiii12 <santi.agudo12@gmail.com>
2023-10-18 14:30:01 +0200

    0.4

---
+++ src/components/MovieList.svelte
@@ -0,0 +1,40 @@
+<script>
+    const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=es-US&page=1&sort_by=popularity.desc'
+    const options = {
+       method: 'GET',
+       headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YmQwMjc1NGUzZWFiMzljMDQzZmU3ZmI5NTY2N2QzMyIsInN1YiI6IjY1MmY4NWE5ZWE4NGM3MDEwYzFkZmYxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8pnPkKuzkh0AHZAtIxS0gFWYP8aDc7zrz-FcekkqWlo'
+  }
+};
+
+let movies = [];
+
+fetch(url, options)
+  .then(response => response.json())
+  .then( data => movies = data.results)
+  .catch(err => console.error(err));
+
+</script>
+<div>
+{ #each movies as movie }
+       <img alt="Cover" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}>
+{/each}
+
+</div>
+
+<style>
+    div {
+        display: flex;
+        flex-wrap:wrap;
+        justify-content: center;
+        gap: 15px;
+    }
+
+    img {
+     height :121px;
+     width:215px;
+     object-fit: cover;
+
+    }
+</style>
